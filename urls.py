08e36bca2a53fd5067cec733766e090296f1f2b6
mick <mick@uwkm.nl>
2021-01-19 20:23:59 +0100

    add auth templates

--- urls.py
+++ urls.py
@@ -1,10 +1,8 @@
 from django.contrib.auth import views as auth_views
-from django.urls import path
+from django.urls import path, include
 from account import views
 
 urlpatterns = [
-    # path("login/", views.user_login, name="login"),
-    path("login/", auth_views.LoginView.as_view(), name="login"),
-    path("logout/", auth_views.LogoutView.as_view(), name="logout"),
+    path("", include("django.contrib.auth.urls")),
     path("", views.dashboard, name="dashboard"),
 ]
