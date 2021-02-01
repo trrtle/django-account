a48261f3897cb67d0166a29f496e31d8dbe9b48d
Genbu94 <mmaarnoudse@outlook.com>
2021-02-01 21:18:49 +0100

    add black & pyint

--- urls.py
+++ urls.py
@@ -1,4 +1,3 @@
-from django.contrib.auth import views as auth_views
 from django.urls import path, include
 from account import views
 
@@ -6,5 +5,5 @@ urlpatterns = [
     path("", include("django.contrib.auth.urls")),
     path("", views.dashboard, name="dashboard"),
     path("register/", views.register, name="register"),
-    path("edit/", views.edit, name='edit')
+    path("edit/", views.edit, name="edit"),
 ]
