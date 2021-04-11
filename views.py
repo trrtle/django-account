e78b76e44a98513fb293dd6cf4664a0b43f4b972
Genbu94 <mmaarnoudse@outlook.com>
2021-04-11 14:02:04 +0200

    add utlis, modify requirements and userregistrationform

--- views.py
+++ views.py
@@ -22,9 +22,9 @@ def register(request):
             new_user.save()
             Profile.objects.create(user=new_user)
             return render(request, "account/register_done.html", {"new_user": new_user})
-    else:
-        user_form = UserRegistrationForm()
-        return render(request, "account/register.html", {"user_form": user_form})
+
+    user_form = UserRegistrationForm()
+    return render(request, "account/register.html", {"user_form": user_form})
 
 
 @login_required
